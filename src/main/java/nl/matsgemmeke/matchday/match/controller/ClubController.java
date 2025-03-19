691fb805b92eccc7e7058fc10258d017e23b7cd4
Mats Gemmeke <mats.gemmeke@alliander.com>
2025-03-19 14:20:55 +0100

    Added logging to console

--- src/main/java/nl/matsgemmeke/matchday/match/controller/ClubController.java
+++ src/main/java/nl/matsgemmeke/matchday/match/controller/ClubController.java
@@ -1,6 +1,7 @@
 package nl.matsgemmeke.matchday.match.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nl.matsgemmeke.matchday.match.controller.dto.ClubDTO;
 import nl.matsgemmeke.matchday.match.service.ClubService;
 import org.springframework.http.ResponseEntity;
@@ -11,17 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/club")
 @RequiredArgsConstructor
+@Slf4j
 public class ClubController {
 
     private final ClubService clubService;
 
     @GetMapping("/")
     public ResponseEntity<List<ClubDTO>> getAllClubs() {
+        log.info("Handling GET request: getAllClubs");
         return ResponseEntity.ok(clubService.getAllClubs());
     }
 
     @PostMapping("/")
     public ResponseEntity<Void> postClub(@RequestBody ClubDTO clubDTO) {
+        log.info("Handling POST request: postClub with body " + clubDTO);
         clubService.createClub(clubDTO);
         return ResponseEntity.accepted().build();
     }
