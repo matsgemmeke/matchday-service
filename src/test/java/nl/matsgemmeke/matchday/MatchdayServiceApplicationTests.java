5a3548e7bd5d277d222b48382db98c06016e8df0
Mats Gemmeke <mats.gemmeke@alliander.com>
2025-03-24 16:11:10 +0100

    Add chatgpt pr review job

--- src/test/java/nl/matsgemmeke/matchday/MatchdayServiceApplicationTests.java
+++ src/test/java/nl/matsgemmeke/matchday/MatchdayServiceApplicationTests.java
@@ -3,14 +3,11 @@ package nl.matsgemmeke.matchday;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Fail.fail;
-
 @SpringBootTest
 class MatchdayServiceApplicationTests {
 
 	@Test
 	void thisShouldFail() {
-		fail("kapot");
 	}
 
 }
